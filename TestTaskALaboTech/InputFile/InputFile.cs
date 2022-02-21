539536eeab3cf6caa0dd3e29b209a47280b6f5fb
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2022-02-21 16:42:54 +0600

    Some work on test.

---
+++ TestTaskALaboTech/InputFile/InputFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestTaskALaboTech.InputFile
+{
+    public static class InputFile
+    {
+        public static bool TryParse(this string inputString, out List<int> chips)
+        {
+            chips = new List<int>();
+
+            if (inputString.StartsWith("chips: [") && inputString.EndsWith(']'))
+            {
+                inputString = inputString.Replace("chips: [", "");
+                inputString = inputString.Replace("]", "");
+
+                string[] inputValues = inputString.Split(", ");
+
+                foreach (var item in inputValues)
+                {
+                    if (int.TryParse(item, out int value))
+                    {
+                        chips.Add(value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect line format");
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect line format");
+                return false;
+            }
+        }
+    }
+}
--- TestTaskALaboTech/Program.cs
+++ TestTaskALaboTech/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using TestTaskALaboTech.InputFile;
 
 namespace TestTaskALaboTech
 {
@@ -7,7 +8,34 @@ namespace TestTaskALaboTech
     {
         static void Main(string[] args)
         {
-            Console.ReadKey();
+            Console.WriteLine("Jose set up a circular poker table for his friends,\n" +
+                              "so that each of the seats at the table has the same number of poker chips.\n" +
+                              "But when Jose wasn’t looking, someone rearranged all of the chips,\n" +
+                              "so that they are no longer evenly distributed!\n" +
+                              "Now Jose needs to redistribute the chips,\n" +
+                              "so that every seat has the same number before his friends arrive.\n" +
+                              "But Jose is very meticulous: to ensure that he doesn’t lose any chips in the process,\n" +
+                              "he only moves chips between adjacent seats.\n" +
+                              "Moreover, he only moves chips one at a time.What is the minimum number of chip moves\n" +
+                              "Jose will need to make to bring the chips back to equilibrium?");
+            Console.WriteLine("(Input format: 'chips: [1, 5, 9, 10, 5]')");
+
+            while (true)
+            {
+                Console.WriteLine("Input:");
+                string inputString = Console.ReadLine();
+
+                if (inputString.TryParse(out List<int> chips))
+                {
+                    foreach (var item in chips)
+                        Console.WriteLine(item);
+                    Console.WriteLine("Output:");
+                }
+
+
+            }
         }
+
+
     }
 }
