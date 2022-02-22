a830c524c9b3d0e9abd8a77e227c39977828d554
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2022-02-22 21:35:53 +0600

    Add base plan. Preparation to optimization plan.

--- TestTaskALaboTech/Transport/TransportChips.cs
+++ TestTaskALaboTech/Transport/TransportChips.cs
@@ -14,52 +14,52 @@ namespace TestTaskALaboTech.TransportTask
             // Количество мест, в дальнейшем для расчета
             int quantityPlaces = table.Count;
 
-            // Заводим счетчик перемещения фишек
-            int counter = 0;
 
-            while (table.Min() != table.Max())
+            //foreach (var item in table)
+            //    Console.Write(item + " ");
+            //Console.WriteLine();
+
+            // Заводим листы поставщиков и потребителей
+            // Первый индекс места за столом, второе кол-во фишек необходимое или излишнее
+            List<Tuple<int, double>> suppliers = new List<Tuple<int, double>>();
+            List<Tuple<int, double>> consumers = new List<Tuple<int, double>>();
+
+            // Заполняем листы поставщиков и потребителей
+            for (int i = 0; i < table.Count; i++)
             {
-                //foreach (var item in table)
-                //    Console.Write(item + " ");
-                //Console.WriteLine();
+                if (table[i] > equilibrium)
+                    suppliers.Add(new Tuple<int, double>(i, table[i] - equilibrium));
+                if (table[i] < equilibrium)
+                    consumers.Add(new Tuple<int, double>(i, equilibrium - table[i]));
+            }
 
-                // Заводим листы поставщиков и потребителей
-                // Первый индекс места за столом, второе кол-во фишек необходимое или излишнее
-                List<Tuple<int, int>> suppliers = new List<Tuple<int, int>>();
-                List<Tuple<int, int>> consumers = new List<Tuple<int, int>>();
+            /*Console.WriteLine("suppliers");
+            foreach (var item in suppliers)
+                Console.WriteLine("place: " + item.Item1 + " chips: " + item.Item2);
+            Console.WriteLine("consumers");
+            foreach (var item in consumers)
+                Console.WriteLine("place: " + item.Item1 + " chips: " + item.Item2);*/
 
-                // Заполняем листы поставщиков и потребителей
-                for (int i = 0; i < table.Count; i++)
-                {
-                    if (table[i] > equilibrium)
-                        suppliers.Add(new Tuple<int, int>(i, table[i] - equilibrium));
-                    if (table[i] < equilibrium)
-                        consumers.Add(new Tuple<int, int>(i, equilibrium - table[i]));
-                }
+            // Строим матрицу цен транспортировки
+            int[,] pricesOfTransport = new int[suppliers.Count, consumers.Count];
 
-                /*Console.WriteLine("suppliers");
-                foreach (var item in suppliers)
-                    Console.WriteLine("place: " + item.Item1 + " chips: " + item.Item2);
-                Console.WriteLine("consumers");
-                foreach (var item in consumers)
-                    Console.WriteLine("place: " + item.Item1 + " chips: " + item.Item2);*/
+            for (int i = 0; i < suppliers.Count; i++)
+                for (int j = 0; j < consumers.Count; j++)
+                    pricesOfTransport[i, j] = Math.Min(
+                        Math.Abs(suppliers[i].Item1 - consumers[j].Item1),
+                        quantityPlaces - Math.Abs(suppliers[i].Item1 - consumers[j].Item1));
 
-                // Строим матрицу цен транспортировки
-                int[,] pricesOfTransport = new int[suppliers.Count, consumers.Count];
+            // Подготавливаем матрицу доставки
+            double[,] quantityOfTransport = new double[suppliers.Count, consumers.Count];
+            for (int i = 0; i < suppliers.Count; i++)
+                for (int j = 0; j < consumers.Count; j++)
+                    quantityOfTransport[i, j] = double.NaN;
 
-                for (int i = 0; i < suppliers.Count; i++)
-                {
-                    for (int j = 0; j < consumers.Count; j++)
-                    {
-                        pricesOfTransport[i, j] = Math.Min(
-                            Math.Abs(suppliers[i].Item1 - consumers[j].Item1),
-                            quantityPlaces - Math.Abs(suppliers[i].Item1 - consumers[j].Item1));
-                        //Console.Write(pricesOfTransport[i, j]);
-                    }
-                    //Console.WriteLine();
-                }
-
+            // Работаем пока не сформируем опорный план
+            while (true)
+            {
                 // Ищем перевозку по минимальной стоимости
+                // С незаблокированной осью
                 int min = int.MaxValue;
                 int indexOfSupplier = 0, indexOfConsumer = 0;
 
@@ -67,7 +67,9 @@ namespace TestTaskALaboTech.TransportTask
                 {
                     for (int j = 0; j < consumers.Count; j++)
                     {
-                        if (min > pricesOfTransport[i, j])
+                        if (min > pricesOfTransport[i, j] 
+                            && !double.IsNaN(suppliers[i].Item2)
+                            && !double.IsNaN(consumers[j].Item2))
                         {
                             min = pricesOfTransport[i, j];
                             indexOfSupplier = i;
@@ -76,23 +78,60 @@ namespace TestTaskALaboTech.TransportTask
                     }
                 }
 
-                //Console.WriteLine("indexOfSupplier: " + indexOfSupplier);
-                //Console.WriteLine("indexOfConsumer: " + indexOfConsumer);
-                //Console.WriteLine("supplier: " + suppliers[indexOfSupplier].Item1);
-                //Console.WriteLine("consumer: " + consumers[indexOfConsumer].Item1);
+                if (min == int.MaxValue)
+                    break;
 
-                // Перемещаем фишку
-                table[suppliers[indexOfSupplier].Item1]--;
-                table[consumers[indexOfConsumer].Item1]++;
+                Console.WriteLine("indexOfSupplier == " + indexOfSupplier);
+                Console.WriteLine("indexOfConsumer == " + indexOfConsumer);
+                Console.WriteLine("quantitySup == " + suppliers[indexOfSupplier].Item2);
+                Console.WriteLine("quantityCon == " + consumers[indexOfConsumer].Item2);
 
-                // Добавляем количество произведенных перемещений
-                counter += pricesOfTransport[indexOfSupplier, indexOfConsumer];
+                if (suppliers[indexOfSupplier].Item2 >= consumers[indexOfConsumer].Item2)
+                {
+                    quantityOfTransport[indexOfSupplier, indexOfConsumer] = consumers[indexOfConsumer].Item2;
 
-                //Console.WriteLine("counter: " + counter);
-                //Console.WriteLine("___________________________");
+                    suppliers[indexOfSupplier] = new Tuple<int, double>
+                        (suppliers[indexOfSupplier].Item1, 
+                         suppliers[indexOfSupplier].Item2 - consumers[indexOfConsumer].Item2);
+                    consumers[indexOfConsumer] = new Tuple<int, double>
+                        (consumers[indexOfConsumer].Item1,
+                         double.NaN);
+                }
+                else
+                {
+                    quantityOfTransport[indexOfSupplier, indexOfConsumer] = suppliers[indexOfSupplier].Item2;
+
+                    consumers[indexOfConsumer] = new Tuple<int, double>
+                        (consumers[indexOfConsumer].Item1,
+                         consumers[indexOfConsumer].Item2 - suppliers[indexOfSupplier].Item2);
+                    suppliers[indexOfSupplier] = new Tuple<int, double>
+                        (suppliers[indexOfSupplier].Item1,
+                         double.NaN);
+                }
+
+                Console.WriteLine("suppliers");
+                foreach (var item in suppliers)
+                    Console.WriteLine("place: " + item.Item1 + " chips: " + item.Item2);
+                Console.WriteLine("consumers");
+                foreach (var item in consumers)
+                    Console.WriteLine("place: " + item.Item1 + " chips: " + item.Item2);
             }
 
-            return counter;
+            int priceOfTransport = 0;
+
+            for (int i = 0; i < suppliers.Count; i++)
+            {
+                for (int j = 0; j < consumers.Count; j++)
+                {
+                    Console.Write(quantityOfTransport[i, j]);
+
+                    if (!double.IsNaN(quantityOfTransport[i, j]))
+                        priceOfTransport += pricesOfTransport[i, j] * (int)quantityOfTransport[i, j];
+                }
+                Console.WriteLine();
+            }
+
+            return priceOfTransport;
         }
 
     }
